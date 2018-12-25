89e436c71a99d109b579e7a485b98ae58ff8f316
yunus-neg <35871957+yunus-neg@users.noreply.github.com>
2018-12-25 04:02:17 +0300

    Revert "add AStar"
    This reverts commit 45cfa91fe6cd3a18a01a14b974257b0c9ec9d86b.

--- qu.js
+++ qu.js
@@ -1,107 +1,96 @@
-const nodes = [
-  {
-    11:[1550,1260,1140,1280,390,1140,1040,921,918,1070,1470,0]
-  },
-  {
-    index: 0,
-    name: "GATE",
-    connectedPoints: [
-      { index: 2, distance: 1400 },
-      { index: 9, distance: 1600 }
-    ],
-    lat: 26.346945,
-    lng: 43.774606
-  },
-  {
-    index: 1,
-    name: "Sciences",
-    connectedPoints: [{ index: 9, distance: 500 }, { index: 3, distance: 170 }],
-    lat: 26.347276376753985,
-    lng: 43.76750390308882
-  },
-  {
-    index: 2,
-    name: "Hospital",
-    connectedPoints: [{ index: 0, distance: 1600 }],
-    lat: 26.3511399,
-    lng: 43.7739468
-  },
-  {
-    index: 3,
-    name: "Engineering",
-    connectedPoints: [
-      { index: 1, distance: 170 },
-      { index: 5, distance: 300 },
-      { index: 10, distance: 290 }
-    ],
-    lat: 26.347013,
-    lng: 43.7660243
-  },
-  {
-    index: 4,
-    name: "Conference",
-    connectedPoints: [
-      { index: 7, distance: 1000 },
-      { index: 11, distance: 500 }
-    ],
-    lat: 26.3551067,
-    lng: 43.7653139
-  },
-  {
-    index: 5,
-    name: "Sharia",
-    connectedPoints: [{ index: 6, distance: 300 }, { index: 3, distance: 300 }],
-    lat: 26.3482436,
-    lng: 43.7648052
-  },
-  {
-    index: 6,
-    name: "Arabic",
-    connectedPoints: [{ index: 5, distance: 300 }, { index: 7, distance: 550 }],
-    lat: 26.3492627,
-    lng: 43.7648615
-  },
-  {
-    index: 7,
-    name: "Medicine",
-    connectedPoints: [
-      { index: 4, distance: 1000 },
-      { index: 6, distance: 550 },
-      { index: 8, distance: 210 }
-    ],
-    lat: 26.35028,
-    lng: 43.7669114
-  },
-  {
-    index: 8,
-    name: "Pharmacy",
-    connectedPoints: [{ index: 9, distance: 250 }, { index: 7, distance: 210 }],
-    lat: 26.3504489,
-    lng: 43.7678961
-  },
-  {
-    index: 9,
-    name: "ECO",
-    connectedPoints: [
-      { index: 0, distance: 1600 },
-      { index: 8, distance: 250 },
-      { index: 1, distance: 500 }
-    ],
-    lat: 26.3491522,
-    lng: 43.7684959
-  },
-  {
-    index: 10,
-    name: "Preparatory",
-    connectedPoints: [{ index: 3, distance: 290 }],
-    lat: 26.3454236,
-    lng: 43.7643794
-  },
-  {
-    index: 11,
-    name: "Computer",
-    connectedPoints: [{ index: 4, distance: 500 }],
-    lat: 26.3584409,
-    lng: 43.7659686
-  }
-];
+const nodes =
+    [
+
+        {
+            "index": 0,
+            "name": "GATE",
+            "connectedPoints": [2, 2],
+            "lat": 26.348167,
+            "lng": 43.773193
+
+        },
+        {
+            "index": 1,
+            "name": "Sciences",
+            "connectedPoints": [10,3],
+            "lat": 26.347276376753985,
+            "lng": 43.76750390308882
+        },
+        {
+            "index": 2,
+            "name": "Hospital",
+            "connectedPoints": [0],
+            "lat": 26.3511399,
+            "lng": 43.7739468
+        },
+        {
+            "index": 3,
+            "name": "Engineering",
+            "connectedPoints": [1,5,11],
+            "lat": 26.347013,
+            "lng": 43.7660243
+        },
+        {
+            "index": 4,
+            "name": "Conference",
+            "connectedPoints": [8,12],
+            "lat": 26.3551067,
+            "lng": 43.7653139
+        },
+        {
+            "index": 5,
+            "name": "Sharia",
+            "connectedPoints": [6,3],
+            "lat": 26.3482436,
+            "lng": 43.7648052
+        },
+        {
+            "index": 6,
+            "name": "Arabic",
+            "connectedPoints": [5,7],
+            "lat": 26.3492627,
+            "lng": 43.7648615
+        },
+        {
+            "index": 7,
+            "name": "Dentistry",
+            "connectedPoints": [6,8],
+            "lat": 26.3504355,
+            "lng": 43.7661208
+        },
+        {
+            "index": 8,
+            "name": "Medicine",
+            "connectedPoints": [4,7],
+            "lat": 26.35028,
+            "lng": 43.7669114
+        },
+        {
+            "index": 9,
+            "name": "Pharmacy",
+            "connectedPoints": [10,8],
+            "lat": 26.3504489,
+            "lng": 43.7678961
+        },
+        {
+            "index": 10,
+            "name": "ECO",
+            "connectedPoints": [0,9,1],
+            "lat": 26.3491522,
+            "lng": 43.7684959
+        },
+        {
+            "index": 11,
+            "name": "Preparatory",
+            "connectedPoints": [3],
+            "lat": 26.3454236,
+            "lng": 43.7643794
+        },
+        {
+            "index": 12,
+            "name": "Computer",
+            "connectedPoints": [4],
+            "lat": 26.3584409,
+            "lng": 43.7659686
+        }
+    ]
